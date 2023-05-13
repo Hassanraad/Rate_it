13bfea353b302e8e9c59fd5ec7182064a4529ef9
Hassan <mohmad1987mohmad1987@gmail.com>
2023-05-13 18:59:07 +0300

    add fonts style and work on the footer

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "./footer.css";
-export default function Footer (){
+export default function Footer() {
     return(<>
-    <div>
-        <h1 className="logo">Rate it</h1>
+    <div className="footer">
+       
         <footer>
 
-<div></div>
-<div></div>
-<div></div>
+<div className="s01">
+    <p>More about company</p>
+    <p>Makers masarat Graduation project</p>
+</div>
+<div className="s02" >
+<h1 className="logo">Rate it</h1>
+    <span><img src="" alt="" /><a href="#">Facebook</a></span>
+    <span><img src="" alt="" /><a href="#">Instgram</a></span>
+    <span><img src="" alt="" /><a href="#">Twitter</a></span>
+</div>
+<div className="s03"> 
+    <span><img src="" alt="" /><span>AL makers</span></span>
+    <span><img src="" alt="" /><span>projact@gamil.com</span></span>
+    <span><img src="" alt="" /><span>07728167699</span></span>
+</div>
 </footer>
     </div>
     
