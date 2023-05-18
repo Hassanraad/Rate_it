fd54b015d7a10dfc7d05a3679207b0a5329fcecc
Hassan <mohmad1987mohmad1987@gmail.com>
2023-05-18 17:51:44 +0300

    add icons and more style to footer

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./footer.css";
+import facebook from "../assets/imgs/facebook.png"
+import insta from "../assets/imgs/instagram.png"
+import Twitter from "../assets/imgs/twitter.png"
+
 export default function Footer() {
     return(<>
     <div className="footer">
@@ -7,14 +11,15 @@ export default function Footer() {
         <footer>
 
 <div className="s01">
-    <p>More about company</p>
-    <p>Makers masarat Graduation project</p>
+    <p className="p1">More about company</p>
+    <p className="p2">Makers masarat Graduation project</p>
 </div>
 <div className="s02" >
 <h1 className="logo">Rate it</h1>
-    <span><img src="" alt="" /><a href="#">Facebook</a></span>
-    <span><img src="" alt="" /><a href="#">Instgram</a></span>
-    <span><img src="" alt="" /><a href="#">Twitter</a></span>
+    <span className="s04">Social media</span>
+    <span className="span1"><img src={facebook} alt="" width={24} height={24} /><a href="#" className="a1">Facebook</a></span>
+    <span className="span1"><img src={insta} alt="" width={24} height={24} /><a href="#"className="a1">Instgram</a></span>
+    <span className="span1" ><img src={Twitter} alt="" width={24} height={24} /><a href="#"className="a1">Twitter</a></span>
 </div>
 <div className="s03"> 
     <span><img src="" alt="" /><span>AL makers</span></span>
