602c7bb953469ce0940dbc650a173e6a6f5e2617
Hassan <mohmad1987mohmad1987@gmail.com>
2023-05-25 03:33:53 +0300

    update yarn

---
+++ src/pages/companypage.jsx
@@ -0,0 +1,37 @@
+import './companypage.css';
+export default function Company(){
+
+
+    return (
+        <>
+        <div>
+            <div className='ad00'>
+            <div className='ad01'>
+                <div>
+                    <p>Have a 20% discount for the first order from pizza Hut</p>
+                    <a href="#">Order now</a>
+                </div>
+            </div>
+            </div>
+            <div>
+                <h1>Comments</h1>
+                <span>
+                    <a href=""><img src="" alt="" /></a>
+                    <input type="text" placeholder='add comment'  />
+                </span>
+                <div>
+                <span>
+                <a href=""><img src="" alt="" /></a>
+                <h2>name</h2>
+                </span>
+                <span><i></i></span>
+                <div>
+                    <p>jkwflsajlfd</p>
+                </div>
+                </div>
+            </div>
+        </div>
+        
+        </>
+    )
+}
