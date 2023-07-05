f2a0e04446a50382eefb2a6351c0cfd62953ce4a
Hassan <mohmad1987mohmad1987@gmail.com>
2023-07-05 22:32:15 +0300

    complete route to sliderpage and company page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import Animation from './components/Animation';
-import First from './components/explore/firstpage/First';
-import Sec from './components/explore/secondpage/Sec';
-import Navbar from './layout/Navbar';
-import Register from './components/register/Register';
 import React, { Suspense, lazy } from 'react';
 import { Route, Routes,Router } from 'react-router-dom';
 import './components/Animation.css';
 import Slider from './components/slider'
-import  Login  from "./components/register/Login";
 import "./components/register/Form1.css"
 import './components/explore/Navcss.css'
 import './components/explore/secondpage/Sec.css';
 import './components/explore/firstpage/First.css';
-
-
-import Layout from './layout/footer';
+import Layout from './layout/layout';
 import Company from './pages/companypage';
 
 function App() {
@@ -22,12 +14,12 @@ function App() {
   return (
 
     <div className="App">
-      <Navbar/>
-      <First />
-     <Sec/> 
-       <Register/>
-      <Layout/>
-   
+      <Routes>
+        <Route element={<Layout/>}>
+        <Route index element={<Slider/>}/>
+        <Route path='/company' element={<Company/>}/>
+        </Route>
+      </Routes>
    </div>
   );
 }
--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -10,7 +10,6 @@ return(
     <div>
     <First/>
     <Sec/>
-    <Animation/>
     <Register/>
     </div>
 </>
