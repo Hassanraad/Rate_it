1df152a344365fe6dca32281e608dd1104cd2001
Hassan <mohmad1987mohmad1987@gmail.com>
2023-07-07 12:26:13 +0300

    complete merge with main

--- src/App.js
+++ src/App.js
@@ -6,14 +6,7 @@ import "./components/register/Form1.css"
 import './components/explore/Navcss.css'
 import './components/explore/secondpage/Sec.css';
 import './components/explore/firstpage/First.css';
-<<<<<<< HEAD
 import Layout from './layout/layout';
-=======
-import Footer from './layout/footer';
-
-
-import Layout from './layout/footer';
->>>>>>> b1c7a8cced7e839c88ace9169b3d70c0950dcd14
 import Company from './pages/companypage';
 
 function App() {
@@ -21,27 +14,12 @@ function App() {
   return (
 
     <div className="App">
-<<<<<<< HEAD
       <Routes>
         <Route element={<Layout/>}>
         <Route index element={<Slider/>}/>
         <Route path='/company' element={<Company/>}/>
         </Route>
       </Routes>
-=======
-      <Navbar />
-      
-      <Animation />
-      
-      <Register />
-      <Footer />
-      <Navbar/>
-      <First />
-     <Sec/> 
-       <Register/>
-      <Layout/>
-   
->>>>>>> b1c7a8cced7e839c88ace9169b3d70c0950dcd14
    </div>
   );
 }
