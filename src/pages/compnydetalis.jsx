84e92f348b467415c545153c1243726e5e2faaeb
Hassan <mohmad1987mohmad1987@gmail.com>
2023-07-10 19:07:55 +0300

    workeng on header of company page

---
+++ src/pages/compnydetalis.jsx
@@ -0,0 +1,53 @@
+import './compnydetalis.css';
+export default function Detalis() {
+    return(
+    <>
+        <div className="header0001">
+                <div className='header0002'>
+                    <div>
+                        <span><p className='pam00000001'>Pizza Hut</p></span>
+                    </div>
+                    <div>
+                        <span><p className='pam00000003'>Pizza Hut one of best restaurant in the world </p></span>
+                    </div>
+                    <div>
+                            <span><p className='pam00000002'>location : AL_Jadriya</p></span>
+                    </div>
+                    <div>
+                            <span><p className='pam00000002'> order in: 07728167699</p></span>
+                    </div>
+                </div>
+        </div>
+        <div className=''>
+            <div><p className='pam00000004'> The most requested meals</p></div>
+            <ul>
+                <li>
+            <div>
+                <div>
+                    
+                </div>
+                <div>
+                    <div>
+                        <span>
+
+                        </span>
+                        <span>
+
+                        </span>
+                    </div>
+                    <div>
+                        <span>
+                        
+                        </span>
+                        <span>
+
+                        </span>
+                    </div>
+                </div>
+            </div>
+            </li>
+            </ul>
+        </div>
+    </>
+    )
+}
